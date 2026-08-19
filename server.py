892783fb3a88831e5a3ca4d717ddbcaf6d11e25d
RTXgamer180 <raphael1622013@gmail.com>
2026-08-19 20:45:46 +0200

    Update server.py

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ from pathlib import Path
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
 MOVIE_FOLDER = "MOVIES_DC1"
-PORT = int(os.environ.get("PORT", 5000))
+PORT = int(os.environ.get("PORT", 8080))
 LOGO_EXTENSIONS = [".png", ".jpg", ".jpeg", ".webp"]
 VIDEO_EXTENSIONS = [".mp4"]
 LINK_EXTENSIONS = [".url", ".link", ".youtube", ".yt"]
